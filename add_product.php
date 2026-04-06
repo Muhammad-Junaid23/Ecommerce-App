1ab8100057ae770a44c8396e9422446916352edd
Muhammad-Junaid23 <mj23.muhammadjunaid@gmail.com>
2026-04-06 19:58:34 +0500

    CRUD + cart

---
+++ add_product.php
@@ -0,0 +1,19 @@
+<?php 
+require 'config.php';
+
+if($_SERVER["REQUEST_METHOD"]== "POST"){
+    $name =$_POST['name'];
+    $price =$_POST['price'];
+
+    $stmt = $pdo->prepare("INSERT INTO products (name,price) VALUES (?,?)");
+    $stmt->execute([$name,$price]);
+
+    echo "Product Added!";
+}
+?>
+
+<form method="POST">
+    <input name="name" placeholder="Product Name" required><br>
+    <input name="price" placeholder="Price" required><br>
+    <button>Add Product</button>
+</form>
